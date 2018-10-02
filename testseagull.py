aa1804a6e7e34c5d39b53fe609b06e6f93d6b78b
16Aghnar <34051973+16Aghnar@users.noreply.github.com>
2018-10-02 11:18:45 +0200

    end kmeans

--- testseagull.py
+++ testseagull.py
@@ -9,8 +9,6 @@ import matplotlib.pyplot as plt
 import cv2
 from sklearn.cluster import KMeans
 
-seagull = cv2.imread("seagull.jpg")
-# seagull[:,:,2], seagull[:,:,1] = seagull[:,:,1], seagull[:,:,2]
  
 '''
 plt.figure()
@@ -22,14 +20,12 @@ plt.imshow(seagull[:,:,1])
 plt.subplot(3,1,3)
 plt.imshow(seagull[:,:,2])
 '''
-sdim = seagull.shape
-print(sdim)
-lowdim = (sdim[0]//2, sdim[1]//2, 3)
-print(lowdim)
+
 def rescale_frame(frame, scale=0.5):
     width = int(frame.shape[1] * scale)
     height = int(frame.shape[0] * scale)
     dim = (width, height)
+    print(dim)
     return cv2.resize(frame, dim, interpolation =cv2.INTER_AREA)
 
 def invert_rb(img):
@@ -38,20 +34,31 @@ def invert_rb(img):
     img[:,:,0], img[:,:,2] = img2, img0
     return img
 
-seagull = rescale_frame(seagull)
-seagull = invert_rb(seagull)
-seagull = np.reshape(seagull, newshape=(lowdim[0]*lowdim[1],-1))
-kmeans = KMeans(12).fit(seagull)
-clusteredseagull = np.zeros((lowdim[0], lowdim[1], 3))
-clusterlabels = np.reshape(kmeans.labels_, newshape=(lowdim[0], lowdim[1]))
+def run_clustering(img, nbclust=3):
+    
+    kmeans = KMeans(12).fit(img)
+    clusteredimg = np.zeros((lowdim[0], lowdim[1], 3))
+    clusterlabels = np.reshape(kmeans.labels_, newshape=(lowdim[0], lowdim[1]))
+    
+    centroids = kmeans.cluster_centers_
+    
+    for i in range(lowdim[0]):
+        for j in range(lowdim[1]):
+            clusteredimg[i,j] = centroids[clusterlabels[i,j],:] 
+    return clusteredimg, centroids
 
-centroids = kmeans.cluster_centers_
-
-for i in range(lowdim[0]):
-    for j in range(lowdim[1]):
-        clusteredseagull[i,j] = centroids[clusterlabels[i,j],:] 
-
-plt.imshow(clusteredseagull)
-print(centroids)
-cv2.imwrite("kmeansseagull_4.jpg", clusteredseagull)
-print(seagull.shape)
+if __name__=='__main__':
+    
+    seagull = cv2.imread("kmeansseagull_4.jpg")
+    # seagull[:,:,2], seagull[:,:,1] = seagull[:,:,1], seagull[:,:,2]
+    
+    # seagull = rescale_frame(seagull, scale=0.5)
+    seagull = invert_rb(seagull)
+    lowdim = seagull.shape
+    # seagull = np.reshape(seagull, newshape=(lowdim[0]*lowdim[1],-1))
+    
+    # clusteredseagull, centroids = run_clustering(seagull, nbclust=3)
+    # plt.imshow(clusteredseagull)
+    # print(centroids)
+    cv2.imwrite("kmeansseagull_4bis.jpg", seagull)
+    print(seagull.shape)
